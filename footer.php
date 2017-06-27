e701231f14d081f4654e93f86eb76263d81089d0
obyron88 <CODA2017@yahoo.com>
2017-06-27 13:45:59 +0200

    fist commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>
+ <a style="color:#0a93a6; text-decoration:none;" href="#"> © Enedis 2017
+</a>Médiateur National de l’Énergie</p>
+    </footer>
+  </body>
+  </html>
---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="./bootstrap-3.3.7-dist/css/bootstrap-theme.css" rel="stylesheet">
+      <link href="./bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+      <link href="./bootstrap-3.3.7-dist/fonts" rel="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great%7CSource+Sans+Pro" rel="stylesheet">
+      <script src="jquery-3.2.1.js"></script>
+      <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+      <link rel="stylesheet"href="styleHF.css"/>
+      <title>Enedis</title>
+<body>
+  <header>
+    <div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
+  <!-- Overlay -->
+  <!-- <div class="overlay"></div> -->
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#bs-carousel" data-slide-to="1"></li>
+    <li data-target="#bs-carousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item slides active">
+      <div class="slide-1"></div>
+      <div class="hero">
+        <hgroup>
+            <h1><a href="index.php"><img src="logo3.png"></a></h1>
+            <h3>Label Eco-consommation</h3>
+        </hgroup>
+      </div>
+    </div>
+    <div class="item slides">
+      <div class="slide-2"></div>
+      <div class="hero">
+        <hgroup>
+            <h1><a href="index.php"><img src="logo3.png"></a></h1>
+            <h3>Label Eco-consommation</h3>
+        </hgroup>
+      </div>
+    </div>
+    <div class="item slides">
+      <div class="slide-3"></div>
+      <div class="hero">
+        <hgroup>
+            <h1><a href="index.php"><img src="logo3.png"></a></h1>
+            <h3>Label Eco-consommation</h3>
+        </hgroup>
+      </div>
+    </div>
+  </div>
+</div>
+</header>
