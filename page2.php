f7a6c388f5abc9187ed39403b7d11f0fda8b095a
Rémy <remdub@live.fr>
2017-06-27 18:06:05 +0200

    add page2 layout

--- page2.php
+++ page2.php
@@ -1,7 +1,26 @@
 <?php
 include("header.php")
  ?>
+<h2 class="town">Ville de </h2>
 
+<img src="http://lorempixel.com/output/abstract-q-c-300-300-3.jpg" alt="etiquette">
+
+<section class="result">
+    <p>Cette ville a atteint le stade de ville responsable, elle reçoit donc l'étiquette energetique JAUNE</p>
+</section>
+
+<div class="table-responsive">
+
+ <table class="table table-hover table-bordered" id="data">
+ </table>
+
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        
+    });
+</script>
  <?php
  include("footer.php")
   ?>
