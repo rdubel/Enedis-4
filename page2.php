5b1b853139339c4426d7623ba8df11b0bff667bd
raphael <moudens.raphael@orange.fr>
2017-06-28 10:38:04 +0200

    tableau comparatif conso/pro

--- page2.php
+++ page2.php
@@ -1,7 +1,177 @@
 <?php
 include("header.php")
- ?>
+?>
 
- <?php
- include("footer.php")
-  ?>
+
+<img class="amp" src="/static/img/ampoulerouge.png" alt="">
+<img class="amp" src="/static/img/ampolejaune.png" alt="">
+<img class="amp" src="/static/img/ampouleorange.png" alt="">
+<img class="amp" src="/static/img/ampouleverte.png" alt="">
+<h3 class="titre">Ville de Mende</h3>
+<table class="table table-striped table-bordered">
+	<h4>Production/consommation mensuelle de la ville de Mende en 2016</h4>
+	<thead> 
+		<tr> 
+			<td>Mois</td> 
+			<td>Janvier</td> 
+			<td>Février</td> 
+			<td>Mars</td> 
+			<td>Avril</td> 
+			<td>Mai</td> 
+			<td>Juin</td> 
+			<td>Juillet</td> 
+			<td>Août</td> 
+			<td>Septembre</td> 
+			<td>Octobre</td> 
+			<td>Novembre</td> 
+			<td>Décembre</td> 
+		</tr> 
+	</thead> 
+	<tbody> 
+		<tr> 
+			<td>Consommation (MWH)</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td>  
+		</tr> 
+		<tr> 
+			<td>Production (MWH)</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+		<tr> 
+			<td>Température Maxi</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+		<tr> 
+			<td>Température Mini</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+	</tbody> 
+</table>
+<table class="table table-striped table-bordered">
+	<h4>Production/consommation mensuelle de la ville de Mende en 2015</h4>
+	<thead> 
+		<tr> 
+			<td>Mois</td> 
+			<td>Janvier</td> 
+			<td>Février</td> 
+			<td>Mars</td> 
+			<td>Avril</td> 
+			<td>Mai</td> 
+			<td>Juin</td> 
+			<td>Juillet</td> 
+			<td>Août</td> 
+			<td>Septembre</td> 
+			<td>Octobre</td> 
+			<td>Novembre</td> 
+			<td>Décembre</td> 
+		</tr> 
+	</thead> 
+	<tbody> 
+		<tr> 
+			<td>Consommation (MWH)</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td>  
+		</tr> 
+		<tr> 
+			<td>Production (MWH)</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+		<tr> 
+			<td>Température Maxi</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+		<tr> 
+			<td>Température Mini</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+			<td>17</td> 
+		</tr> 
+	</tbody> 
+</table>
+<?php
+include("footer.php")
+?>
