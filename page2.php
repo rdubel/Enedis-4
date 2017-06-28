dee95de3b50b0f80d8fe6702423bca631a7af6b6
Rémy <remdub@live.fr>
2017-06-28 12:44:59 +0200

    add JSON file and populate page2 tables with it's data

--- page2.php
+++ page2.php
@@ -10,168 +10,88 @@ include("header.php")
 <h3 class="titre">Ville de Mende</h3>
 <table class="table table-striped table-bordered">
 	<h4>Production/consommation mensuelle de la ville de Mende en 2016</h4>
-	<thead> 
-		<tr> 
-			<td>Mois</td> 
-			<td>Janvier</td> 
-			<td>Février</td> 
-			<td>Mars</td> 
-			<td>Avril</td> 
-			<td>Mai</td> 
-			<td>Juin</td> 
-			<td>Juillet</td> 
-			<td>Août</td> 
-			<td>Septembre</td> 
-			<td>Octobre</td> 
-			<td>Novembre</td> 
-			<td>Décembre</td> 
-		</tr> 
-	</thead> 
-	<tbody> 
-		<tr> 
-			<td>Consommation (MWH)</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td>  
-		</tr> 
-		<tr> 
-			<td>Production (MWH)</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-		<tr> 
-			<td>Température Maxi</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-		<tr> 
-			<td>Température Mini</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-	</tbody> 
+	<thead>
+		<tr class="months">
+            <td>Mois :</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="conso2016">
+			<td>Consommation (MWH)</td>
+		</tr>
+		<tr class="prod2016">
+			<td>Production (MWH)</td>
+		</tr>
+		<tr class="tempmax2016">
+			<td>Température Maxi</td>
+		</tr>
+		<tr class="tempmin2016">
+			<td>Température Mini</td>
+		</tr>
+	</tbody>
 </table>
 <table class="table table-striped table-bordered">
 	<h4>Production/consommation mensuelle de la ville de Mende en 2015</h4>
-	<thead> 
-		<tr> 
-			<td>Mois</td> 
-			<td>Janvier</td> 
-			<td>Février</td> 
-			<td>Mars</td> 
-			<td>Avril</td> 
-			<td>Mai</td> 
-			<td>Juin</td> 
-			<td>Juillet</td> 
-			<td>Août</td> 
-			<td>Septembre</td> 
-			<td>Octobre</td> 
-			<td>Novembre</td> 
-			<td>Décembre</td> 
-		</tr> 
-	</thead> 
-	<tbody> 
-		<tr> 
-			<td>Consommation (MWH)</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td>  
-		</tr> 
-		<tr> 
-			<td>Production (MWH)</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-		<tr> 
-			<td>Température Maxi</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-		<tr> 
-			<td>Température Mini</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-			<td>17</td> 
-		</tr> 
-	</tbody> 
+	<thead>
+		<tr class="months">
+            <td>Mois :</td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="conso2015">
+			<td>Consommation (MWH)</td>
+		</tr>
+		<tr class="prod2015">
+			<td>Production (MWH)</td>
+		</tr>
+		<tr class="tempmax2015">
+			<td>Température Maxi</td>
+		</tr>
+		<tr class="tempmin2015">
+			<td>Température Mini</td>
+		</tr>
+	</tbody>
 </table>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var monthsRow = $(".months");
+        var conso2016 = $(".conso2016");
+        var prod2016 = $(".prod2016");
+        var conso2015 = $(".conso2015");
+        var prod2015 = $(".prod2015");
+        var tempMax2016 = $(".tempmax2016");
+        var tempMin2016 = $(".tempmin2016");
+        var tempMax2015 = $(".tempmax2015");
+        var tempMin2015 = $(".tempmin2015");
+        $.getJSON('./static/fakedata.json', function(data) {
+            console.log(data.conso_mensuelle_2016.janvier);
+                for (var month in data.conso_mensuelle_2016) {
+                    monthsRow.append("<td>"+month+"</td>");
+                    conso2016.append("<td>"+ data.conso_mensuelle_2016[month] +"</td>")
+                }
+                for (var month in data.prod_mensuelle_2016) {
+                    prod2016.append("<td>"+data.prod_mensuelle_2016[month]+"</td>")
+                }
+                for (var month in data.temp_mois_2016) {
+                    console.log(data.temp_mois_2016[month].max);
+                    tempMax2016.append('<td>'+ data.temp_mois_2016[month].max +'</td>');
+                    tempMin2016.append('<td>'+ data.temp_mois_2016[month].min +'</td>');
+                }
+
+                for (var month in data.conso_mensuelle_2015) {
+                    conso2015.append("<td>"+ data.conso_mensuelle_2015[month] +"</td>")
+                }
+                for (var month in data.prod_mensuelle_2015) {
+                    prod2015.append("<td>"+data.prod_mensuelle_2015[month]+"</td>")
+                }
+                for (var month in data.temp_mois_2015) {
+                    console.log(data.temp_mois_2015[month].max);
+                    tempMax2015.append('<td>'+ data.temp_mois_2015[month].max +'</td>');
+                    tempMin2015.append('<td>'+ data.temp_mois_2015[month].min +'</td>');
+                }
+        });
+    });
+</script>
 <?php
 include("footer.php")
 ?>
